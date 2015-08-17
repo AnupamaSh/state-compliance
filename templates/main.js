0620e9f7c47af43a1845a6617354d6ce58a0cd50
AnupamaSh <AnupamaSh@users.noreply.github.com>
2015-08-17 13:13:42 -0400

    Update main.js

--- templates/main.js
+++ templates/main.js
@@ -309,13 +309,108 @@ $("#clickme").click(function(){
 // f) IF more than 40,000 THEN $6000 + $30,000
 
 
-                  $('body').on('click', '#getvalueSC1', function() {
-                    var SaraValue = $("#SaraInput").val();
-                    if (SaraValue<=2500) {
-                        $('#SaraFee').text('Your Fee: $7,000')} else if (2500<SaraValue<9999){
-                        $('#SaraFee').text('Your Fee: $9,000')}else if (SaraValue==10000){
-                        $('#SaraFee').text('Your Fee: $11,000')}else if (10001<SaraValue<=20000){
-                        $('#SaraFee').text('Your Fee: $16,000')}else if (20000<SaraValue<=40000){
-                        $('#SaraFee').text('Your Fee: $26,000')}else if (SaraValue>40000){
-                        $('#SaraFee').text('Your Fee: $36,000')}
-                });
+  $('body').on('click', '#getvalueSC1', function() {
+    var SaraValue = $("#SaraInput").val();
+    if (SaraValue<=2500) {
+        $('#SaraFee').text('Your Fee: $7,000')} else if (2500<SaraValue<9999){
+        $('#SaraFee').text('Your Fee: $9,000')}else if (SaraValue==10000){
+        $('#SaraFee').text('Your Fee: $11,000')}else if (10001<SaraValue<=20000){
+        $('#SaraFee').text('Your Fee: $16,000')}else if (20000<SaraValue<=40000){
+        $('#SaraFee').text('Your Fee: $26,000')}else if (SaraValue>40000){
+        $('#SaraFee').text('Your Fee: $36,000')}
+});
+
+
+///////////Pop-up////////////
+
+
+ $(function() {
+    var dialog, form,
+ 
+      // From http://www.whatwg.org/specs/web-apps/current-work/multipage/states-of-the-type-attribute.html#e-mail-state-%28type=email%29
+      emailRegex = /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/,
+      name = $( "#name" ),
+      email = $( "#email" ),
+      password = $( "#password" ),
+      allFields = $( [] ).add( name ).add( email ).add( password ),
+      tips = $( ".validateTips" );
+ 
+    function updateTips( t ) {
+      tips
+        .text( t )
+        .addClass( "ui-state-highlight" );
+      setTimeout(function() {
+        tips.removeClass( "ui-state-highlight", 1500 );
+      }, 500 );
+    }
+ 
+    function checkLength( o, n, min, max ) {
+      if ( o.val().length > max || o.val().length < min ) {
+        o.addClass( "ui-state-error" );
+        updateTips( "Length of " + n + " must be between " +
+          min + " and " + max + "." );
+        return false;
+      } else {
+        return true;
+      }
+    }
+ 
+    function checkRegexp( o, regexp, n ) {
+      if ( !( regexp.test( o.val() ) ) ) {
+        o.addClass( "ui-state-error" );
+        updateTips( n );
+        return false;
+      } else {
+        return true;
+      }
+    }
+ 
+    function addUser() {
+      var valid = true;
+      allFields.removeClass( "ui-state-error" );
+ 
+      valid = valid && checkLength( name, "username", 3, 16 );
+      valid = valid && checkLength( email, "email", 6, 80 );
+      valid = valid && checkLength( password, "password", 5, 16 );
+ 
+      valid = valid && checkRegexp( name, /^[a-z]([0-9a-z_\s])+$/i, "Username may consist of a-z, 0-9, underscores, spaces and must begin with a letter." );
+      valid = valid && checkRegexp( email, emailRegex, "eg. ui@jquery.com" );
+      valid = valid && checkRegexp( password, /^([0-9a-zA-Z])+$/, "Password field only allow : a-z 0-9" );
+ 
+      if ( valid ) {
+        $( "#users tbody" ).append( "<tr>" +
+          "<td>" + name.val() + "</td>" +
+          "<td>" + email.val() + "</td>" +
+          "<td>" + password.val() + "</td>" +
+        "</tr>" );
+        dialog.dialog( "close" );
+      }
+      return valid;
+    }
+ 
+    dialog = $( "#dialog-form" ).dialog({
+      autoOpen: false,
+      height: 300,
+      width: 550,
+      modal: true,
+      buttons: {
+        "Add": addUser,
+        Cancel: function() {
+          dialog.dialog( "close" );
+        }
+      },
+      close: function() {
+        form[ 0 ].reset();
+        allFields.removeClass( "ui-state-error" );
+      }
+    });
+ 
+    form = dialog.find( "form" ).on( "submit", function( event ) {
+      event.preventDefault();
+      addUser();
+    });
+ 
+    $( "#create-user" ).button().on( "click", function() {
+      dialog.dialog( "open" );
+    });
+  });
